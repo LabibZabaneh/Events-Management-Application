321f9ff885eed6546aa45c069420801db4485932
LabibZabaneh <labibraedzabaneh@gmail.com>
2024-02-15 16:57:56 +0000

    Added CRUD user http methods to users controller

--- user-event-microservice/src/main/java/com/event/management/events/controllers/UsersController.java
+++ user-event-microservice/src/main/java/com/event/management/events/controllers/UsersController.java
@@ -2,13 +2,18 @@ package com.event.management.events.controllers;
 
 import com.event.management.events.domain.Event;
 import com.event.management.events.domain.User;
+import com.event.management.events.dto.UserDTO;
 import com.event.management.events.repositories.UsersRepository;
 import io.micronaut.data.annotation.Repository;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Post;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.*;
 import jakarta.inject.Inject;
 
-@Repository
+import javax.transaction.Transactional;
+import java.net.URI;
+import java.util.Optional;
+
+@Controller("/users")
 public class UsersController {
 
     @Inject
@@ -19,8 +24,63 @@ public class UsersController {
         return repo.findAll();
     }
 
+    @Post("/")
+    public HttpResponse<Void> addUser(@Body UserDTO dto){
+        User u = new User();
+        u.setFirstName(dto.getFirstName());
+        u.setLastName(dto.getLastName());
+        u.setEmail(dto.getEmail());
+        u.setDateOfBirth(dto.getDateOfBirth());
+        u.setMobileNumber(dto.getMobileNumber());
+        repo.save(u);
+
+        return HttpResponse.created(URI.create("/users/" + u.getId()));
+    }
+
     @Get("/{id}")
     public User getUser(long id) {
         return repo.findById(id).orElse(null);
+    }
+
+    @Transactional
+    @Put("/{id}")
+    public  HttpResponse<Void> updateUser(long id, @Body UserDTO dto){
+        Optional<User> oUser = repo.findById(id);
+        if (oUser.isEmpty()){
+            return HttpResponse.notFound();
+        }
+        User u = updateUserAttributes(oUser.get(), dto);
+        repo.update(u);
+        return HttpResponse.ok();
+    }
+
+    @Transactional
+    @Delete("/{id}")
+    public HttpResponse<Void> deleteUser(long id) {
+        Optional<User> oUser = repo.findById(id);
+        if (oUser.isEmpty()){
+            return HttpResponse.notFound();
+        }
+        repo.delete(oUser.get());
+        return HttpResponse.ok();
+    }
+
+    private User updateUserAttributes(User u, UserDTO dto) {
+        if (dto.getFirstName() != null){
+            u.setFirstName(dto.getFirstName());
+        }
+        if (dto.getLastName() != null){
+            u.setLastName(dto.getLastName());
+        }
+        if (dto.getEmail() != null){
+            u.setEmail(dto.getEmail());
+        }
+        if (dto.getMobileNumber() != null){
+            u.setMobileNumber(dto.getMobileNumber());
+        }
+        if (dto.getDateOfBirth() != null){
+            u.setDateOfBirth(dto.getDateOfBirth());
+        }
+        return u;
     }
 }
--- user-event-microservice/src/main/java/com/event/management/events/dto/UserDTO.java
+++ user-event-microservice/src/main/java/com/event/management/events/dto/UserDTO.java
@@ -5,17 +5,26 @@ import io.micronaut.serde.annotation.Serdeable;
 @Serdeable
 public class UserDTO {
 
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
     private String dateOfBirth;
     private String mobileNumber;
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
