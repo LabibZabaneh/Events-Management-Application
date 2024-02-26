89493103ca67a7239aa8ae7b664ce120b6e36440
LabibZabaneh <labibraedzabaneh@gmail.com>
2024-02-26 00:06:21 +0000

    Added more Event entity attributes (registration microservice)

--- registration-microservice/src/main/java/com/event/management/registrations/domain/Event.java
+++ registration-microservice/src/main/java/com/event/management/registrations/domain/Event.java
@@ -1,5 +1,6 @@
 package com.event.management.registrations.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.micronaut.serde.annotation.Serdeable;
 
 import javax.persistence.Column;
@@ -18,11 +19,25 @@ public class Event {
     @Column(nullable = false)
     private String eventName;
 
+    @Column(nullable = false)
+    private String venue;
+
+    @Column(nullable = false)
+    private String date;
+
+    @Column(nullable = false)
+    private String time;
+
+    @JsonIgnore
     @ManyToMany
     private Set<User> registeredUsers;
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getEventName() {
@@ -39,5 +54,29 @@ public class Event {
 
     public void setRegisteredUsers(Set<User> registeredUsers) {
         this.registeredUsers = registeredUsers;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 }
