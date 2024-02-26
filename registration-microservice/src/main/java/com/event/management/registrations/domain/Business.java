f2faceb6223ba51eadaa5df04a8fc37002882854
LabibZabaneh <labibraedzabaneh@gmail.com>
2024-02-26 00:05:00 +0000

    Added Business entity class (registration microservice)

--- registration-microservice/src/main/java/com/event/management/registrations/domain/Business.java
+++ registration-microservice/src/main/java/com/event/management/registrations/domain/Business.java
@@ -1,4 +1,49 @@
 package com.event.management.registrations.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.micronaut.serde.annotation.Serdeable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@Serdeable
+@Entity
 public class Business {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @JsonIgnore
+    @ManyToMany
+    private Set<User> followers;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(Set<User> followers) {
+        this.followers = followers;
+    }
 }
