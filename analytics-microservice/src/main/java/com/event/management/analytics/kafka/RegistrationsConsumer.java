84240bd1e1babd13497b2549a990318efb48c6e7
LabibZabaneh <labibraedzabaneh@gmail.com>
2024-03-07 10:57:40 +0000

    chore: added functionality to Registrations Consumer class(analytics-microservice)

--- analytics-microservice/src/main/java/com/event/management/analytics/kafka/RegistrationsConsumer.java
+++ analytics-microservice/src/main/java/com/event/management/analytics/kafka/RegistrationsConsumer.java
@@ -1,8 +1,15 @@
 package com.event.management.analytics.kafka;
 
+import com.event.management.analytics.domain.Event;
+import com.event.management.analytics.domain.User;
+import com.event.management.analytics.repositories.EventsRepository;
+import com.event.management.analytics.repositories.UsersRepository;
 import io.micronaut.configuration.kafka.annotation.KafkaKey;
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.Topic;
+import jakarta.inject.Inject;
+
+import java.util.Optional;
 
 @KafkaListener
 public class RegistrationsConsumer {
@@ -10,13 +17,43 @@ public class RegistrationsConsumer {
     final String USER_REGISTERED_TOPIC = "user-registered";
     final String USER_UNREGISTERED_TOPIC = "user-unregistered";
 
+    @Inject
+    UsersRepository usersRepo;
+
+    @Inject
+    EventsRepository eventsRepo;
+
     @Topic(USER_REGISTERED_TOPIC)
     public void registerUser(@KafkaKey Long userId, Long eventId){
-        System.out.println("User with id " + userId + " registered to event with id " + eventId);
+        Optional<User> oUser = usersRepo.findById(userId);
+        Optional<Event> oEvent = eventsRepo.findById(eventId);
+        if (oUser.isPresent() && oEvent.isPresent()){
+            User user = oUser.get();
+            user.addRegistration();
+            usersRepo.update(user);
+
+            Event event = oEvent.get();
+            event.addRegistration();
+            eventsRepo.update(event);
+
+            System.out.println("User with id " + userId + "registered to event with id " + eventId);
+        }
     }
 
     @Topic(USER_UNREGISTERED_TOPIC)
     public void unregisterUser(@KafkaKey Long userId, Long eventId){
-        System.out.println("User with id " + userId + " unregistered to event with id " + eventId);
+        Optional<User> oUser = usersRepo.findById(userId);
+        Optional<Event> oEvent = eventsRepo.findById(eventId);
+        if (oUser.isPresent() && oEvent.isPresent()){
+            User user = oUser.get();
+            user.removeRegistration();
+            usersRepo.update(user);
+
+            Event event = oEvent.get();
+            event.deleteRegistration();
+            eventsRepo.update(event);
+
+            System.out.println("User with id " + userId + "unregistered to event with id " + eventId);
+        }
     }
 }
