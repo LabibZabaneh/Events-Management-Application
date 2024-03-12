7a517c9722fec0de9837e5fe04a2e07138cd71f5
LabibZabaneh <labibraedzabaneh@gmail.com>
2024-03-12 16:21:32 +0000

    chore: Added http get methods to return event age analytics (analytics-microservice)

--- analytics-microservice/src/main/java/com/event/management/analytics/controllers/TrendingController.java
+++ analytics-microservice/src/main/java/com/event/management/analytics/controllers/TrendingController.java
@@ -1,5 +1,6 @@
 package com.event.management.analytics.controllers;
 
+import com.event.management.analytics.domain.AgeCount;
 import com.event.management.analytics.domain.Event;
 import com.event.management.analytics.domain.Organizer;
 import com.event.management.analytics.repositories.EventsRepository;
@@ -10,6 +11,7 @@ import io.micronaut.http.annotation.Get;
 import jakarta.inject.Inject;
 
 import javax.transaction.Transactional;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,6 +40,52 @@ public class TrendingController {
             return null;
         }
         return organizersRepo.findTopRegisteredEventsByOrganizerId(id, 10);
+    }
+
+    @Get("/events")
+    public Iterable<Event> getEvents(){
+        return eventsRepo.findAll();
+    }
+
+    @Transactional
+    @Get("/events/{id}/age-distribution")
+    public List<AgeCount> getEventAgeDistribution(long id){
+        Optional<Event> oEvent = eventsRepo.findById(id);
+        if (oEvent.isEmpty()){
+            return Collections.emptyList();
+        }
+        return eventsRepo.findById(id).get().getAgeCounts();
+    }
+
+    @Transactional
+    @Get("/events/{id}/average-age")
+    public double getEventAverageAge(long id){
+        Optional<Event> oEvent = eventsRepo.findById(id);
+        if (oEvent.isEmpty()){
+            return 0.0;
+        }
+        Event event = oEvent.get();
+        int totalRegistrations = event.getRegistrations();
+        if (totalRegistrations == 0){
+            return 0.0;
+        }
+        int totalAge = 0;
+        for (AgeCount ageCount : event.getAgeCounts()){
+            totalAge += ageCount.getAge() * ageCount.getCount();
+        }
+        return (double) totalAge / totalRegistrations;
+    }
+
+    @Transactional
+    @Get("events/{id}/top-age-groups/{limit}")
+    public List<AgeCount> getEventTopAgeGroups(long id, int limit){
+        Optional<Event> oEvent = eventsRepo.findById(id);
+        if (oEvent.isEmpty()){
+            return Collections.emptyList();
+        }
+        List<AgeCount> ageCounts = oEvent.get().getAgeCounts();
+        ageCounts.sort((a1, a2) -> a2.getCount() - a1.getCount());
+        return ageCounts.subList(0, Math.min(limit, ageCounts.size()));
     }
 
     @Get("/popular/events")
