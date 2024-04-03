c570ab71e35e97c757e7d48a3e4cea542a6a0765
LabibZabaneh <labibraedzabaneh@gmail.com>
2024-04-03 22:39:24 +0100

    feat: added payments-microservice

---
+++ payments-microservice/src/main/java/com/event/management/payments/Application.java
@@ -0,0 +1,10 @@
+package com.event.management.payments;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
